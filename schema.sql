5e9d443168be023227ff50b88033a8758277ffeb
Fletcher LaRue <laruefletcher@gmail.com>
2018-11-28 21:32:52 -0800

    implements caching for latlong and for weather

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE locations;
+DROP TABLE locations, weathers;
 
 CREATE TABLE IF NOT EXISTS locations (
   id SERIAL PRIMARY KEY,
@@ -6,4 +6,12 @@ CREATE TABLE IF NOT EXISTS locations (
   formatted_query VARCHAR(255),
   latitude NUMERIC(8,6),
   longitude NUMERIC(9,6)
+);
+
+CREATE TABLE IF NOT EXISTS weathers (
+  id SERIAL PRIMARY KEY,
+  forecast VARCHAR(255),
+  time VARCHAR(255),
+  created_at BIGINT,
+  location_id INTEGER NOT NULL REFERENCES locations(id)
 );
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ client.on('error', (err) => console.error(err));
 // let test = client.query('SELECT * FROM locations')
 //   .then( (response) => console.log(response));
 // console.log(test);
-console.log('~~~~~~~~~');
+// console.log('~~~~~~~~~');
 
 
 // Application Setup
@@ -61,23 +61,17 @@ function Location(query, res) {
 Location.table_name = 'locations';
 
 Location.lookupLocation = (location) => {
-
-  // console.log('location params: ', location);
+  // console.log(`checking the database for ${location.query}`);
   const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
-  // const SQL = `SELECT * FROM $1 WHERE search_query=$2;`;
-  // const values = [Location.table_name, location.query];
   const values = [location.query];
-  // console.log('values: ', values);
-  // console.log('SQL: ', SQL);
 
   client.query(SQL, values)
     .then( (dbResponse) => {
-      // console.log('dbResponse: ', dbResponse);
-      if (dbResponse.rowsCount > 0){
-        console.log('dbResponse.rowsCount > 0: ', dbResponse.rowsCount > 0);
+      if (dbResponse.rowCount > 0){
+        console.log('cache hit');
         location.cacheHit(dbResponse);
       } else {
-        console.log('no rows detected, in the else statement');
+        console.log('cache miss');
         location.cacheMiss();
       }
     })
@@ -86,29 +80,37 @@ Location.lookupLocation = (location) => {
 
 Location.prototype = {
   save: function() {
-    // query , formatted query, lat, long
-    // const SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
     console.log('saving the data');
-    const SQL = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);`
+    const SQL = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;`;
     const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
-    // console.log('SQL: ', SQL);
-    // console.log('values: ', values);
     return client.query(SQL, values)
       .then( (result) => {
-        console.log('result after inserting: ', result);
-        console.log('this: ', this);
-        console.log('result.rows: ', result.rows);
-        console.log('result.rows[0]: ', result.rows[0]);
-        // this.id = result.rows[0].id;
-        // console.log('this: ', this);
+        this.id = result.rows[0].id;
         return this;
       })
   }
 }
 
 function Weather(day) {
+  this.tableName = 'weathers';
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
+  this.created_at = Date.now();
+  console.log('new weather with created at: ', this.created_at);
+}
+
+Weather.tableName = 'weathers';
+Weather.lookup = lookup;
+Weather.deleteByLocationId = deleteByLocationId;
+
+Weather.prototype = {
+  save: function(location_id) {
+    // save data
+    console.log('saving weather data');
+    const SQL = `INSERT INTO ${this.tableName} (forecast, time, created_at, location_id) VALUES ($1, $2, $3, $4);`;
+    const values = [this.forecast , this.time , this.created_at, location_id];
+    client.query(SQL, values);
+  }
 }
 
 function Yelp(restaurant) {
@@ -180,6 +182,7 @@ function getLocation(request, response) {
     query: request.query.data,
 
     cacheHit: function(result) {
+      console.log('sending response to user');
       response.send(result.rows[0]);
     },
 
@@ -189,10 +192,9 @@ function getLocation(request, response) {
       superagent.get(url)
         .then((res) => {
           const location = new Location(this.query, res);
-          console.log('our new location: ', location);
+          // console.log('our new location: ', location);
           location.save()
             .then((location) => {
-              console.log('test');
               return response.send(location);
             });
         })
@@ -201,24 +203,44 @@ function getLocation(request, response) {
   });
 }
 
-// function searchToLatLong(query) {
-//   console.log('making lat/long API request');
-//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-//   return superagent.get(url)
-//     .then((res) => {
-//       return new Location(query, res);
-//     })
-//     .catch(error => handleError(error));
-// }
-
 function getWeather(request, response) {
-  // console.log('weather route hit');
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-  superagent.get(url)
-    .then((result) => {
-      response.send( result.body.daily.data.map( (day) => new Weather(day)) );
-    })
-    .catch(error => handleError(error, response));
+  console.log(`Weather route hit, location ID is: ${request.query.data.id}`);
+  Weather.lookup({
+    location: request.query.data.id,
+    tableName: Weather.tableName,
+    cacheHit: function(result) {
+      // check if valid
+      let ageOfResultsInMinutes = (Date.now() - result.rows[0].created_at) / (1000 * 60);
+      if (ageOfResultsInMinutes > 30){
+        // remove stale data
+        // console.log(`detected old data, age: ${ageOfResultsInMinutes}`);
+        deleteByLocationId(Weather.tableName, request.query.data.id);
+        // add new data
+        this.cacheMiss();
+      } else {
+        // data is valid, send to client
+        // console.log('fresh data, no need to for API call, sending data to user')
+        response.send(result.rows);
+      }
+    },
+    cacheMiss: function() {
+      // get the data
+      console.log('making weather API request');
+      const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+      return superagent.get(url)
+        .then( (result) => {
+          // make and save all the new weathers
+          const allWeathers =  result.body.daily.data.map( (day) => {
+            const weather = new Weather(day) // create new weather objects
+            weather.save(request.query.data.id); // save it, over-writing old data
+            return weather;
+          });
+          console.log('sending weather to client');
+          response.send(allWeathers); // send it to the client
+        })
+        .catch(error => handleError(error));
+    }
+  });
 }
 
 function getRestaurant(request, response) { 
@@ -265,5 +287,31 @@ function getTrails(request, response) {
     .catch( (error) => handleError(error, response));
 }
 
+function lookup(options) {
+  console.log(`looking up data from: ${options.tableName} route`);
+  const SQL = `SELECT * from ${options.tableName} WHERE location_id=$1`;
+  const values = [options.location];
+  client.query(SQL, values)
+    .then( (response) => {
+      if (response.rowCount > 0){
+        // cache hit
+        // console.log('cache hit')
+        options.cacheHit(response);
+      } else {
+        // cache miss
+        // console.log('cache miss')
+        options.cacheMiss(response);
+      }
+    })
+    .catch( (err) => handleError(err));
+}
 
+function deleteByLocationId(table, city){
+  //sql delete
+  console.log(`deleting a table entry: ${city} from ${table}`);
+  const SQL = `DELETE from ${table} WHERE location_id=${city}`;
+  const response = client.query(SQL);
+  // console.log(`response: ${response}`);
+  return response;
+}
 
